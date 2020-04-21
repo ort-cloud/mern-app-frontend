9ced7f0027e5d911926c6587fb06d5ea594943fe
Carl Ortutay <carl.orty@gmail.com>
2020-04-21 14:32:59 -0400

    added sidebar for mobile

--- src/shared/components/Nav/MainNav.js
+++ src/shared/components/Nav/MainNav.js
@@ -1,24 +1,32 @@
 import React from "react";
 import MainHeader from "./MainHeader";
 import {Link} from "react-router-dom";
-import "./MainNav.css";
 import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import "./MainNav.css";
 
 const MainNav = (props) => {
   return (
-    <MainHeader>
-      <button className='main-nav__menu-btn'>
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-      <h1 className='main-nav__title'>
-        <Link to='/'>YourPlaces</Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className='main-nav__drawer-nav'>
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className='main-nav__menu-btn'>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <h1 className='main-nav__title'>
+          <Link to='/'>YourPlaces</Link>
+        </h1>
+        <nav className='main-nav__header-nav'>
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
