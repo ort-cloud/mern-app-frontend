374b7d1bbd12ed35b351db34d8df9dba8d363a93
Carl Ortutay <carl.orty@gmail.com>
2020-04-21 11:17:50 -0400

    added UserItem component

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -5,7 +5,7 @@ const Users = () => {
   //TODO: Replace USERS with fetch to backend.
   const USERS = [
     {
-      id: u1,
+      id: "u1",
       name: "Carlack",
       image:
         "https://www.google.com/url?sa=i&url=https%3A%2F%2Fstarwars.fandom.com%2Fwiki%2FJar_Jar_Binks%2FLegends&psig=AOvVaw2TpBAtNhyR3l1y5HW_ru1c&ust=1586647952024000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOjfobOC3-gCFQAAAAAdAAAAABAD",
