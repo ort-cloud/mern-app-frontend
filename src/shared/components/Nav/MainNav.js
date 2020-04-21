1af29f7da9bc4d7319de6e77b9ae27e93fd7d6c5
Carl Ortutay <carl.orty@gmail.com>
2020-04-21 13:29:45 -0400

    added nav links

--- src/shared/components/Nav/MainNav.js
+++ src/shared/components/Nav/MainNav.js
@@ -2,6 +2,7 @@ import React from "react";
 import MainHeader from "./MainHeader";
 import {Link} from "react-router-dom";
 import "./MainNav.css";
+import NavLinks from "./NavLinks";
 
 const MainNav = (props) => {
   return (
@@ -14,7 +15,9 @@ const MainNav = (props) => {
       <h1 className='main-nav__title'>
         <Link to='/'>YourPlaces</Link>
       </h1>
-      <nav>...</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
   );
 };
