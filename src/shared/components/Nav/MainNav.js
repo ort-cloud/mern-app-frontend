845544906d1515ec7ac9f634f5cbf25ff13272ef
Carl Ortutay <carl.orty@gmail.com>
2020-04-21 14:57:37 -0400

    hamburger drawer now functional

--- src/shared/components/Nav/MainNav.js
+++ src/shared/components/Nav/MainNav.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import MainHeader from "./MainHeader";
 import {Link} from "react-router-dom";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 import "./MainNav.css";
 
 const MainNav = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
     <React.Fragment>
-      <SideDrawer>
-        <nav className='main-nav__drawer-nav'>
-          <NavLinks />
-        </nav>
-      </SideDrawer>
+      {drawerIsOpen && <Backdrop onClick={closeDrawer}/>}
+      {drawerIsOpen ? (
+        <SideDrawer>
+          <nav className='main-nav__drawer-nav'>
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      ) : null}
       <MainHeader>
-        <button className='main-nav__menu-btn'>
+        <button className='main-nav__menu-btn' onClick={openDrawer}>
           <span></span>
           <span></span>
           <span></span>
